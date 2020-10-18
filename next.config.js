2f7866e5dda08006481188e88918c0f13ec6d916
mohammedbahnini <mohammed.gosoft@gmail.com>
2020-10-18 15:35:46 +0200

    change next start script

--- next.config.js
+++ next.config.js
@@ -14,7 +14,8 @@ const nextSettings = {
 const nextConfig = {
     env : {
         api : 'http://localhost:8000/api',
-        PUBLISHABLE_KEY : 'pk_test_51GuNlgK7kyPZtMq547rhppQ0KeiOs6o49MZkaLaRTT6zZOuhNfp4BSJHEVJDTeJAGzIfgCiCtOtvfXWc1R1V2HDx00Oixzal9g'
+        PUBLISHABLE_KEY : 'pk_test_51GuNlgK7kyPZtMq547rhppQ0KeiOs6o49MZkaLaRTT6zZOuhNfp4BSJHEVJDTeJAGzIfgCiCtOtvfXWc1R1V2HDx00Oixzal9g' , 
+        PORT : 3000
     }
 }
 module.exports = withPlugins([[withSass(), withImages()]],nextConfig);
