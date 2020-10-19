6d8b4a6341b63bb979f9743604b2cc4c1cff0104
mohammedbahnini <63128845+mohammedbahnini@users.noreply.github.com>
2020-10-19 17:48:02 +0100

    fix login issues

--- components/Login.js
+++ components/Login.js
@@ -6,7 +6,7 @@ import { isLoggedIn } from '../helpers/auth';
 import { LoadingOutlined } from '@ant-design/icons';
 import { Form, Input, notification, Alert } from 'antd';
 import { connect } from 'react-redux';
-import { config } from 'dotenv';
+
 
 
 import axios from 'axios';
@@ -46,7 +46,6 @@ class Login extends Component {
             const email = this.state.email;
             const password = this.state.password;
             const remember_me = this.state.rememberMe;
-            config();
             const api_path = `${process.env.HOST}/api/user/login`;
             console.log(process.env.API);
             console.log(api_path);
