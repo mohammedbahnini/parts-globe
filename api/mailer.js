9ee985f93e38dece4806703eb3354df7a5e558c7
mohammedbahnini <63128845+mohammedbahnini@users.noreply.github.com>
2020-10-20 16:13:37 +0100

    fix confirm email issue

--- api/mailer.js
+++ api/mailer.js
@@ -1,55 +1,55 @@
 const hbs = require('nodemailer-express-handlebars');
 const nodemailer = require('nodemailer');
 
-const sendMail =async (reciever,token)=>{
+const sendMail = async (reciever, token) => {
 
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve, reject) => {
 
         const smtpTransport = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: 465,
             secure: true,
             auth: {
-                 type: "OAuth2",
-                 user: "med.dev.checker@gmail.com", 
-                 clientId: `${process.env.GMAIL_CLIENT_ID}`,
-                 clientSecret: `${process.env.GMAIL_CLIENT_SECRET}`,
-                 refreshToken: `${process.env.GMAIL_REFRESH_TOKEN}` , 
-                 accessToken: `${process.env.GMAIL_ACCESS_TOKEN}`
+                type: "OAuth2",
+                user: "med.dev.checker@gmail.com",
+                clientId: `${process.env.GMAIL_CLIENT_ID}`,
+                clientSecret: `${process.env.GMAIL_CLIENT_SECRET}`,
+                refreshToken: `${process.env.GMAIL_REFRESH_TOKEN}`,
+                accessToken: `${process.env.GMAIL_ACCESS_TOKEN}`
             }
-       });
-    
-       smtpTransport.use('compile' ,hbs({
-            viewEngine : {
-                extname : '.hbs',
-                layoutsDir : 'mailTemplate' ,
-                partialsDir : 'mailTemplate' , 
-                defaultLayout : 'mail'
+        });
+
+        smtpTransport.use('compile', hbs({
+            viewEngine: {
+                extname: '.hbs',
+                layoutsDir: 'mailTemplate',
+                partialsDir: 'mailTemplate',
+                defaultLayout: 'mail'
             },
-            viewPath : 'mailTemplate',
-            extName : '.hbs'
+            viewPath: 'mailTemplate',
+            extName: '.hbs'
         }));
-    
+
         const mailOptions = {
             from: "med.dev.checker@gmail.com",
             to: reciever,
             subject: "Email verification",
-            template : 'mail' , 
-            context : {
-                msg : 'Verify your email' , 
-                link : `${process.env.WEBSITE_ORIGINE}/confirm_email?token=${token}`
+            template: 'mail',
+            context: {
+                msg: 'Verify your email',
+                link: `${process.env.WEBSITE_ORIGINE}/confirm_email?token=${token}`
             }
         };
-    
+
 
         smtpTransport.sendMail(mailOptions, (err, response) => {
 
-            if(err) resolve({err});
-            else{
+            if (err) resolve({ err });
+            else {
                 smtpTransport.close();
-                resolve({success : true});
+                resolve({ success: true });
             }
-       });
+        });
 
     });
 
