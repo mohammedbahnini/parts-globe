6f364a23b68bd915fc78f62b6e4ef6d9a3879fda
mohammedbahnini <63128845+mohammedbahnini@users.noreply.github.com>
2021-05-03 11:59:58 +0000

    change db host id

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const config = {
     connectionLimit: 10,
-    host: '72.9.135.10',
+    host: '64.188.2.244',
     user: 'era_creator',
     password: 'Gu35T_2020',
     database: 'era_auto_parts'
